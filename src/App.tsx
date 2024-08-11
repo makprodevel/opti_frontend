daf1408b599815848a8b73faacab649522a25e6c
mak.pro.devel <mak.pro.devel@gmail.com>
2024-08-11 17:00:59 +0300

    add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header.tsx'
 import { Route, Routes } from 'react-router-dom'
 import About from './pages/AboutPage.tsx'
 import ChatPage from './pages/ChatPage.tsx'
+import './i18n.ts'
 
 export default function App() {
   return (
