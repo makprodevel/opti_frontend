557ded426c2d2b866e6839c2b753059fa6222bbc
mak <mak.pro.devel@gmail.com>
2024-08-24 23:25:15 +0300

    optimaze change language

--- src/components/ChangeLanguage.tsx
+++ src/components/ChangeLanguage.tsx
@@ -8,27 +8,24 @@ import { ChevronDownIcon } from '@heroicons/react/24/outline'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-const LS_LANGUAGE = 'language_value'
-
 enum Language {
-  en = 'en',
-  ru = 'ru'
+  en = 'en-EN',
+  ru = 'ru-RU'
 }
 
 const languageOptions = [
-  { id: 1, name: 'English', value: Language.en },
-  { id: 2, name: 'Русский', value: Language.ru }
+  { name: 'English', value: Language.en },
+  { name: 'Русский', value: Language.ru }
 ]
 
 export default function ChangeLanguage() {
   const { i18n } = useTranslation()
   const [selectedLanguage, setSelectedLanguage] = useState<Language>(
-    (localStorage.getItem(LS_LANGUAGE) as Language) || Language.en
+    i18n.language as Language
   )
 
   useEffect(() => {
     i18n.changeLanguage(selectedLanguage)
-    localStorage.setItem(LS_LANGUAGE, selectedLanguage)
   }, [selectedLanguage])
 
   return (
@@ -42,7 +39,7 @@ export default function ChangeLanguage() {
         <ListboxOptions className="absolute mt-2 w-48 rounded-md border border-gray-300 bg-gray-200 py-1 shadow-lg">
           {languageOptions.map((option) => (
             <ListboxOption
-              key={option.id}
+              key={option.value}
               value={option.value}
               className="cursor-pointer px-4 py-2 text-sm hover:bg-blue-100"
             >
